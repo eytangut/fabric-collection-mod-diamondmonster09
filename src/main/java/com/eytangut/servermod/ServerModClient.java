bc2de74756241aa26472e3075bc038e70c96dc31
eytangut <eytangut@gmail.com>
2022-07-10 20:08:14 +0300

    Finished v1.0-1.19

---
+++ src/main/java/com/eytangut/servermod/ServerModClient.java
@@ -0,0 +1,10 @@
+package com.eytangut.servermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ServerModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
